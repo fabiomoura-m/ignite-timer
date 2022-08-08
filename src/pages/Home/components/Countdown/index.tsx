49e5e36fadc78f1f20c37696609ea0692c85d55b
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-08-08 19:06:26 -0300

    Funcionalidades da aplicação - Prop Drilling no React

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -1,6 +1,52 @@
+import { differenceInSeconds } from 'date-fns';
+import { useEffect, useState } from 'react';
 import { Separator, CountDownContainer } from './styles';
 
+interface CountdownProps {
+    activeCycle: any;
+}
+
 export function Countdown() {
+    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+
+    const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+
+    useEffect(() => {
+        let interval: number;
+
+        if (activeCycle) {
+            interval = setInterval(() => {
+                const secondsDifference = differenceInSeconds(
+                    new Date(),
+                    activeCycle.startDate
+                );
+
+                if (secondsDifference >= totalSeconds) {
+                    setCycles(state =>
+                        state.map(cycle => {
+                            if (cycle.id === activeCycleId) {
+                                return {
+                                    ...cycle,
+                                    finishedDate: new Date()
+                                };
+                            } else {
+                                return cycle;
+                            }
+                        })
+                    );
+                    setAmountSecondsPassed(totalSeconds);
+                    clearInterval(interval);
+                } else {
+                    setAmountSecondsPassed(secondsDifference);
+                }
+            }, 1000);
+        }
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [activeCycle, totalSeconds, activeCycleId]);
+
     return (
         <CountDownContainer>
             <span>{minutes[0]}</span>
--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,6 +1,27 @@
+import { useForm } from "react-hook-form";
+import * as zod from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { FormContainer, MinutesAmountInput, TaskInput } from "./styles";
 
+
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa'),
+    minutesAmount: zod
+        .number()
+        .min(5, 'O ciclo precisa ser de no mínimo 5 minutos.')
+        .max(60, 'O ciclo precisa ser de no máximo 60 minutos.')
+});
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
+
 export function NewCycleForm() {
+    const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+        resolver: zodResolver(newCycleFormValidationSchema),
+        defaultValues: {
+            task: '',
+            minutesAmount: 0
+        }
+    });
     return (
         <FormContainerntainer>
             <label htmlFor="task">Vou trabalhar em</label>
@@ -26,7 +47,7 @@ export function NewCycleForm() {
                 type="number"
                 placeholder="00"
                 step={5}
-                min={1}
+                min={5}
                 max={60}
                 disabled={!!activeCycle}
                 {...register('minutesAmount', { valueAsNumber: true })}
